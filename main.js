2dea74135e686c73c41611436eb5e421730e6d00
Serventino <43664729+servenn@users.noreply.github.com>
2020-12-25 05:49:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    
+    $('.p-testing').addClass('animate__animated animate__fadeInUp');
+    $('.P-testing').css({
+       opacity:1,
+    //    animationDelay:'2.5s' 
+    });
+});
