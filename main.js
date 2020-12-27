10d3d4a6a736916c8a0eaa310af7e9f5224e519e
Serventino <43664729+servenn@users.noreply.github.com>
2020-12-27 04:26:44 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
     
+    // navbar section
+    $(window).scroll(function(){
+        if($(window).scrollTop()>0){
+            $('.navbar').addClass('sticky');
+            $('.logo').attr('src','https://img.icons8.com/material/24/ffffff/circled-s.png');
+            
+          
+        }else{
+         $('.navbar').removeClass('sticky'); 
+         $('.logo').attr('src','https://img.icons8.com/material/48/ffffff/circled-s.png');
+        }
+     });
+    
     $('.p-testing').addClass('animate__animated animate__fadeInUp');
     $('.P-testing').css({
        opacity:1,
     //    animationDelay:'2.5s' 
     });
+
 });
