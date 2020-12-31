6bef3b67ede9b7057a63133c7f658eaaf6e7508f
Serventino <43664729+servenn@users.noreply.github.com>
2020-12-31 04:07:15 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -31,26 +31,45 @@ $(document).ready(function(){
     $(window).scroll(function(){
 
             //projects animation
-            var top_of_element = $(".test").offset().top;
-            var bottom_of_element = $(".test").offset().top + $(".test").outerHeight();
-            var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
-            var top_of_screen = $(window).scrollTop();
+            var $ptop = $('.test');
+            
 
-            if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
-                // the element is visible, do something
-                var i = 0;
-                $('.test').each(function(){
+            // var top_of_element = $(".test").offset().top;
+            // var bottom_of_element = $(".test").offset().top + $(".test").outerHeight();
+            // var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+            // var top_of_screen = $(window).scrollTop();
+
+            $ptop.each(function(){
+                var topof = $(this).offset().top;
+                var bottomof = $(this).offset().top + $(this).outerHeight();
+                var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+                var top_of_screen = $(window).scrollTop();
+
+                if ((bottom_of_screen > topof) && (top_of_screen < bottomof)){
+                    // the element is visible, do something
+                    var i = 0;
                     $(this).addClass('animate__animated animate__zoomIn');
-                    i+=0.3;
-                    $(this).css({
-                        animationDelay:i+'s',
-                        opacity:1
-                    })
+            
+                }
+
+            })
+
+
+            // if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+            //     the element is visible, do something
+            //     var i = 0;
+            //     $('.test').each(function(){
+            //         $(this).addClass('animate__animated animate__zoomIn');
+            //         i+=0.3;
+            //         $(this).css({
+            //             animationDelay:i+'s',
+            //             opacity:1
+            //         })
                     
 
-                });
+            //     });
         
-            }
+            // }
             // this section belongs to skill bar animation
             var top_of_element = $(".text").offset().top;
             var bottom_of_element = $(".text").offset().top + $(".text").outerHeight();
@@ -60,7 +79,7 @@ $(document).ready(function(){
             if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
 
                 $('.html').addClass('htmls');
-                 setTimeout(show,2000);
+                //  setTimeout(show,2000);
                 $('.css').addClass('csss');
                 $('.js').addClass('jss');
                 $('.java').addClass('javas');
@@ -73,9 +92,7 @@ $(document).ready(function(){
                 
             }
 
-            function show(){
-                document.getElementsByClassName('html').style.innerHTML = '90%';
-            }
+            
             //navbar responsivenes test
             navbar();
         
@@ -92,11 +109,12 @@ $(document).ready(function(){
         if($(window).scrollTop()>0){
             $('.navbar').addClass('sticky');
             $('.logo').attr('src','https://img.icons8.com/material/24/ffffff/circled-s.png');
-
+            // $('.nav-link').addClass('.navbrand');
           
         }else{
          $('.navbar').removeClass('sticky'); 
          $('.logo').attr('src','https://img.icons8.com/material/48/ffffff/circled-s.png');
+        //  $('.nav-link').removeClass('.navbrand');
         }
     }
 
